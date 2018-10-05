68f7f0cb7bf488a6ae4c1fb325ebd3f77435bbaf
unknown <roman.bruns@mail.ru>
2018-10-05 13:20:46 +0300

    void

--- dist/js/main.js
+++ dist/js/main.js
@@ -169,71 +169,71 @@ function templateEngine(jsonData) {
 
 /* Event Handling: Drag & Pinch & Rotate In #cam Image */
 window.onload = function() {
-  if ("ontouchstart" in document.documentElement) {
-    let cam = document.querySelector("#cam");
+  //if ("ontouchstart" in document.documentElement) {
+  let cam = document.querySelector("#cam");
 
-    let conXstart; //start X touch position
-    let imgBackPosition; //img background X position
-    let imgLeft, imgRight, imgTop, imgBot; //img L/R/T/B position
-    let imgCenterX, imgCenterY; //img X&Y center position
-    let touchAngle; //touch rotate angle
-    let touchedPoints = [];
+  let conXstart; //start X touch position
+  let imgBackPosition; //img background X position
+  let imgLeft, imgRight, imgTop, imgBot; //img L/R/T/B position
+  let imgCenterX, imgCenterY; //img X&Y center position
+  let touchAngle; //touch rotate angle
+  let touchedPoints = [];
 
-    if (window.PointerEvent) {
-      cam.addEventListener("pointerdown", startController, false);
-      cam.addEventListener("pointermove", moveController, false);
-      cam.addEventListener("pointerup", stopController, false);
-    }
+  if (window.PointerEvent) {
+    cam.addEventListener("pointerdown", startController, false);
+    cam.addEventListener("pointermove", moveController, false);
+    cam.addEventListener("pointerup", stopController, false);
+  }
 
-    function startController(e) {
-      /* Get Position Info */
-      conXstart = e.clientX;
-      imgBackPosition = parseInt(this.style.backgroundPositionX);
+  function startController(e) {
+    /* Get Position Info */
+    conXstart = e.clientX;
+    imgBackPosition = parseInt(this.style.backgroundPositionX);
 
-      touchedPoints.push({
-        pointerId: e.pointerId,
-        x: e.clientX,
-        y: e.clientY
-      });
+    touchedPoints.push({
+      pointerId: e.pointerId,
+      x: e.clientX,
+      y: e.clientY
+    });
 
-      /* Calculate Center Image */
-      imgLeft = this.getBoundingClientRect().left;
-      imgRight = this.getBoundingClientRect().right;
-      imgTop = this.getBoundingClientRect().top;
-      imgBot = this.getBoundingClientRect().bottom;
+    /* Calculate Center Image */
+    imgLeft = this.getBoundingClientRect().left;
+    imgRight = this.getBoundingClientRect().right;
+    imgTop = this.getBoundingClientRect().top;
+    imgBot = this.getBoundingClientRect().bottom;
 
-      imgWight = parseInt(imgRight - imgLeft);
-      imgHeight = parseInt(imgBot - imgTop);
+    imgWight = parseInt(imgRight - imgLeft);
+    imgHeight = parseInt(imgBot - imgTop);
 
-      imgCenterX = parseInt((imgLeft + imgRight) / 2);
-      imgCenterY = parseInt((imgTop + imgBot) / 2);
-    }
+    imgCenterX = parseInt((imgLeft + imgRight) / 2);
+    imgCenterY = parseInt((imgTop + imgBot) / 2);
+  }
 
-    function moveController(e) {
-      if (touchedPoints.length < 2) {
-        /* Left & Right Move */
-        let xPos = e.clientX;
-        let moveValue = parseInt(xPos - conXstart);
+  function moveController(e) {
+    console.log(touchedPoints);
+    if (touchedPoints.length < 2) {
+      /* Left & Right Move */
+      let xPos = e.clientX;
+      let moveValue = parseInt(xPos - conXstart);
 
-        !imgBackPosition
-          ? (this.style.backgroundPositionX = moveValue + "px")
-          : (this.style.backgroundPositionX =
-              imgBackPosition + moveValue + "px");
-      } else if (touchedPoints.length == 2) {
-        /* если на панели 2 пальца... */
+      !imgBackPosition
+        ? (this.style.backgroundPositionX = moveValue + "px")
+        : (this.style.backgroundPositionX = imgBackPosition + moveValue + "px");
+    } else if (touchedPoints.length == 2) {
+      /* если на панели 2 пальца... */
 
-        /* Rotate */
-        touchAngle = parseInt(
-          Math.atan2(e.clientX - imgCenterX, -(e.clientY - imgCenterY)) *
-            (180 / Math.PI)
-        );
+      /* Rotate */
+      touchAngle = parseInt(
+        Math.atan2(e.clientX - imgCenterX, -(e.clientY - imgCenterY)) *
+          (180 / Math.PI)
+      );
 
-        this.style.transform = "rotate(" + touchAngle + "deg)";
-      }
-    }
-
-    function stopController(e) {
-      touchedPoints = [];
+      this.style.transform = "rotate(" + touchAngle + "deg)";
     }
   }
+
+  function stopController(e) {
+    touchedPoints = [];
+  }
+  //}
 };
