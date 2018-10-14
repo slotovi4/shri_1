c30978dc43ef71e1c70457f8232db7b65d9135c8
unknown <roman.bruns@mail.ru>
2018-10-14 12:47:05 +0300

    added all requests

--- express/express.js
+++ express/express.js
@@ -3,20 +3,13 @@ let app = express();
 const fs = require("fs");
 
 let startDate; //start server date
-let currentDate; //current date
-let difference;
-
-let jsondata;
-let events;
-let typesArray = []; //received types
-let eventsTypes = []; //events types
 let filePath = __dirname + "/api/events.json";
 
 /* Status */
 app.get("/status", function(req, res) {
-  currentDate = new Date(); //get current date
+  let currentDate = new Date(); //get current date
   currentDate = Date.parse(currentDate) / 1000; //convert to seconds
-  difference = currentDate - startDate; //calc difference current date & start server date
+  let difference = currentDate - startDate; //calc difference current date & start server date
 
   /* Get Passed Time */
   let time = new Date(null);
@@ -27,12 +20,13 @@ app.get("/status", function(req, res) {
 });
 
 /* Events */
-app.get("/api/events", function(req, res) {
+app.get("/api/events", function(req, res, next) {
   let typeValues = req.query.type;
 
   /* If Have Param */
   if (typeValues) {
-    typesArray = typeValues.split(":");
+    let typesArray = typeValues.split(":"); //received types
+    let eventsTypes = []; //events types
 
     /* Read Json */
     fs.readFile(filePath, function(err, data) {
@@ -40,10 +34,10 @@ app.get("/api/events", function(req, res) {
         throw err;
       }
 
-      jsondata = JSON.parse(data);
+      let jsondata = JSON.parse(data);
 
       /* Get Event Type */
-      events = jsondata.events;
+      let events = jsondata.events;
       events.forEach(event => {
         if (eventsTypes.indexOf(event.type) == -1) eventsTypes.push(event.type);
       });
@@ -54,15 +48,23 @@ app.get("/api/events", function(req, res) {
           res.status(400).send("incorrect type");
           return;
         } else if (i == typesArray.length - 1) {
-          res.status(404).send("<h1>Page not found</h1>");
-          return;
+          res.send("ok");
         }
       }
     });
-  } else {
+  } else if (req.url === "/api/events") {
     res.sendFile(__dirname + "/api/events.json");
+  } else {
+    next();
   }
 });
+
+/* Other Requests  */
+app.use(err);
+
+function err(req, res) {
+  res.status(404).send("<h1>Page not found</h1>");
+}
 
 /* Start */
 app.listen(8000, function() {
