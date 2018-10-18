d149145b826f9b532af970d3412d6dd5277c9635
unknown <roman.bruns@mail.ru>
2018-10-18 22:25:17 +0300

    void

--- dist/js/main.js
+++ dist/js/main.js
@@ -303,39 +303,6 @@ function touchEvets() {
   }
 }
 
-/* Event handling "click" on the button ".header-mobile-btn": animate button & show/hide mobile menu */
-
-const menu = document.querySelector(".header__menu");
-const menuBtn = document.querySelector(".header-mobile-btn");
-const menuBtnLine = document.querySelectorAll(".header-mobile-btn__line");
-
-/* Set Initial State Of Menu */
-var showMenu = false;
-
-menuBtn.addEventListener("click", toggleMenu);
-
-function toggleMenu() {
-  if (!showMenu) {
-    menu.classList.add("header__menu_show");
-    menuBtn.classList.add("header-mobile-btn_close");
-    menuBtnLine.forEach(item =>
-      item.classList.add("header-mobile-btn__line_close")
-    );
-
-    /* Set Menu State */
-    showMenu = true;
-  } else {
-    menu.classList.remove("header__menu_show");
-    menuBtn.classList.remove("header-mobile-btn_close");
-    menuBtnLine.forEach(item =>
-      item.classList.remove("header-mobile-btn__line_close")
-    );
-
-    /* Set Menu State */
-    showMenu = false;
-  }
-}
-
 /* Draw Canvas Video & Video Effects */
 function canvasVideo(videoId) {
   let video = document.querySelector("#" + videoId); //video
@@ -671,4 +638,37 @@ function soundMuteButton(button, video) {
       button.classList.remove("canv-video-block__sound-mute_active");
     }
   });
+}
+
+/* Event handling "click" on the button ".header-mobile-btn": animate button & show/hide mobile menu */
+
+const menu = document.querySelector(".header__menu");
+const menuBtn = document.querySelector(".header-mobile-btn");
+const menuBtnLine = document.querySelectorAll(".header-mobile-btn__line");
+
+/* Set Initial State Of Menu */
+var showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menu.classList.add("header__menu_show");
+    menuBtn.classList.add("header-mobile-btn_close");
+    menuBtnLine.forEach(item =>
+      item.classList.add("header-mobile-btn__line_close")
+    );
+
+    /* Set Menu State */
+    showMenu = true;
+  } else {
+    menu.classList.remove("header__menu_show");
+    menuBtn.classList.remove("header-mobile-btn_close");
+    menuBtnLine.forEach(item =>
+      item.classList.remove("header-mobile-btn__line_close")
+    );
+
+    /* Set Menu State */
+    showMenu = false;
+  }
 }
