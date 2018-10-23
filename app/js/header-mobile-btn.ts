68f2321335be62ea4aaae81cebe040daa9c6668e
unknown <roman.bruns@mail.ru>
2018-10-23 17:53:01 +0300

    correction header-mobile-btn.ts

--- app/js/header-mobile-btn.ts
+++ app/js/header-mobile-btn.ts
@@ -1,18 +1,18 @@
 /* Event handling "click" on the button ".header-mobile-btn": animate button & show/hide mobile menu */
 
-const menu = document.querySelector(".header__menu");
-const menuBtn = document.querySelector(".header-mobile-btn");
+const menu = <HTMLElement>document.querySelector(".header__menu");
+const menuBtn = <HTMLElement>document.querySelector(".header-mobile-btn");
 const menuBtnLine = document.querySelectorAll(".header-mobile-btn__line");
 
 /* Set Initial State Of Menu */
 let showMenu = false;
 
-menuBtn && menuBtn.addEventListener("click", toggleMenu);
+menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   if (!showMenu) {
-    menu && menu.classList.add("header__menu_show");
-    menuBtn && menuBtn.classList.add("header-mobile-btn_close");
+    menu.classList.add("header__menu_show");
+    menuBtn.classList.add("header-mobile-btn_close");
     menuBtnLine.forEach(item =>
       item.classList.add("header-mobile-btn__line_close")
     );
@@ -20,8 +20,8 @@ function toggleMenu() {
     /* Set Menu State */
     showMenu = true;
   } else {
-    menu && menu.classList.remove("header__menu_show");
-    menuBtn && menuBtn.classList.remove("header-mobile-btn_close");
+    menu.classList.remove("header__menu_show");
+    menuBtn.classList.remove("header-mobile-btn_close");
     menuBtnLine.forEach(item =>
       item.classList.remove("header-mobile-btn__line_close")
     );
